4ac70964920c779f8e41f2dc9bc4930175a48a12
Sylvain <sylvain@sleede.com>
2021-04-27 12:23:16 +0200

    wait for document ready before running the script

--- account/resources/js/script.js
+++ account/resources/js/script.js
@@ -1,3 +1,16 @@
-const rawMembershipDate = document.querySelector('input#rawMembershipDate').value;
-const date = Date.parse(rawMembershipDate);
-document.querySelector('input#membershipDate').value = Intl.DateTimeFormat().format(date);
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}    
+
+docReady(function() {
+    const rawMembershipDate = document.querySelector('input#rawMembershipDate').value;
+    const date = Date.parse(rawMembershipDate);
+    document.querySelector('input#membershipDate').value = Intl.DateTimeFormat().format(date);
+});
+
