7aa0ab50c3f29d110de31f953649ff975a117509
Sylvain <sylvain@sleede.com>
2021-04-27 12:42:50 +0200

    fix ldap date parsing

--- account/resources/js/script.js
+++ account/resources/js/script.js
@@ -6,11 +6,17 @@ function docReady(fn) {
     } else {
         document.addEventListener("DOMContentLoaded", fn);
     }
-}    
+}
+
+function ldapDate(rawDate) {
+    const parsed = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})Z/.exec(rawDate);
+    const month = parseInt(parsed[2], 10) - 1;
+    return new Date(Date.UTC(parsed[1], month, parsed[3], parsed[4], parsed[5]));
+}
 
 docReady(function() {
     const rawMembershipDate = document.querySelector('input#rawMembershipDate').value;
-    const date = Date.parse(rawMembershipDate);
+    const date = ldapDate(rawMembershipDate);
     document.querySelector('input#membershipDate').value = Intl.DateTimeFormat().format(date);
 });
 
